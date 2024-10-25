dd0e4c17edb5290b7c89bc63022d1eb705af21e9
Claude Müller <claude.myller@tele2.com>
2024-10-25 14:19:35 +0200

    Use onedark theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -3,14 +3,20 @@
 -----------------------------------------------------------------------
 
 return {
-  'folke/tokyonight.nvim',
+  -- 'folke/tokyonight.nvim',
+  'navarasu/onedark.nvim',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    -- Load the colorscheme here
-    vim.cmd.colorscheme 'tokyonight-night'
+    require('onedark').setup {
+      style = 'darker',
+    }
+    require('onedark').load()
 
-    -- You can configure highlights by doing something like
-    vim.cmd.hi 'Comment gui=none'
+    -- Load the colorscheme here
+    -- vim.cmd.colorscheme 'onedark'
+    --
+    -- -- You can configure highlights by doing something like
+    -- vim.cmd.hi 'Comment gui=none'
   end,
 }
