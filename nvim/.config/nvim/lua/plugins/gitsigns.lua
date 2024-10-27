49eb6401360b51d8ba9a7edc0336383aa332d106
Claude Müller <${GIT_AUTHOR_EMAL}>
2024-10-27 09:52:04 +0100

    Add windows stuff

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -26,14 +26,22 @@ return {
   },
   config = function()
     require('gitsigns').setup {
+      -- █▓▒░▀▄
       signs = {
-        -- █▓▒░
         add = { text = '█' },
         change = { text = '▓' },
         delete = { text = '▒' },
-        test = 'test',
-        topdelete = { text = '✖' },
-        changedelete = { text = '' },
+        topdelete = { text = '▀' },
+        changedelete = { text = '░' },
+        untracked = { text = '▄' },
+      },
+      signs_staged = {
+        add = { text = '█' },
+        change = { text = '▓' },
+        delete = { text = '▒' },
+        topdelete = { text = '▀' },
+        changedelete = { text = '░' },
+        untracked = { text = '▄' },
       },
       current_line_blame = true,
       current_line_blame_opts = {
