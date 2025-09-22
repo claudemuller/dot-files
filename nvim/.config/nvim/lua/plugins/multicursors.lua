912d3402fb809e1f67e3622777819a42ceaa1b46
Claude Müller <claude.muller@embark-studios.com>
2025-09-22 12:19:34 +0200

    Updates

--- nvim/.config/nvim/lua/plugins/multicursors.lua
+++ nvim/.config/nvim/lua/plugins/multicursors.lua
@@ -5,19 +5,7 @@
 -- Set multicursor
 -- See `:help multicursor`
 return {
-  -- 'smoka7/multicursors.nvim',
-  -- event = 'VeryLazy',
-  -- dependencies = {
-  --   'nvimtools/hydra.nvim',
-  -- },
-  -- opts = {},
-  -- cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
-  -- keys = {
-  --   {
-  --     mode = { 'v', 'n' },
-  --     '<Leader>m',
-  --     '<cmd>MCstart<cr>',
-  --     desc = 'Create a selection for selected text or word under the cursor',
-  --   },
-  -- },
+  'mg979/vim-visual-multi',
+  branch = 'master',
+  opts = {},
 }
