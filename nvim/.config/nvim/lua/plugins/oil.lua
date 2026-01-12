414d92b0cbf9ffbd1f15d08389be70e6afb253e3
Claude Müller <claude.muller@embark-studios.com>
2026-01-12 10:18:55 +0100

    Always show preview in oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -18,6 +18,7 @@ return {
       },
       view_options = {
         show_hidden = true,
+        show_preview = true,
       },
       float = {
         padding = 2,
@@ -27,12 +28,20 @@ return {
         win_options = {
           winblend = 0,
         },
+        preview_split = "left",
       },
       keymaps = {
         ["<ESC>"] = { "actions.close", mode = "n" },
         ["<C-v>"] = { "actions.select", opts = { vertical = true } },
         ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
       },
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilEnter",
+      callback = function()
+        require("oil").open_preview()
+      end,
     })
 
     vim.keymap.set("n", "-", ":Oil --float<CR>", { silent = true })
